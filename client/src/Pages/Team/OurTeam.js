3d48a491d24aa7cbc54ff2313f81152d21914e60
Rohan Magar <rohan.magar.415@gmail.com>
2022-12-09 19:24:10 +0545

    finished our team page UI

--- client/src/Pages/Team/OurTeam.js
+++ client/src/Pages/Team/OurTeam.js
@@ -3,6 +3,8 @@ import Preloader from "../../Components/Preloader/Preloader";
 import "./OurTeam.css";
 import { useNavigate } from "react-router-dom";
 import { BiArrowBack } from "react-icons/bi";
+import { Container, Row, Col } from "react-bootstrap";
+import { Rotate, Fade } from "react-awesome-reveal";
 
 function OurTeam() {
   const navigate = useNavigate();
@@ -16,13 +18,53 @@ function OurTeam() {
   return loading ? (
     <Preloader />
   ) : (
-    <div className="our_team">
+    <Container fluid className="our_team">
       <span onClick={() => navigate(-1)}>
-        <BiArrowBack style={{ marginTop: "-2px" }} />
-        Go Back
+        <BiArrowBack style={{ marginTop: "-2px", marginRight: "5px" }} />
+        Our Team
       </span>
-      <h1>Our Team</h1>
-    </div>
+      <Row>
+        <Col className="col-12 col-md-6 col-lg-4 text-center my-2">
+          <Rotate triggerOnce>
+            <div className="ourteam__image__wrapper">
+              <img
+                src={require("../../Assets/Images/ourteam_rohan-min.jpg")}
+                alt="rohan-pic"
+              />
+            </div>
+          </Rotate>
+          <Fade direction="up" delay={400}>
+            <h3>Rohan Rana Magar</h3>
+          </Fade>
+        </Col>
+        <Col className="col-12 col-md-6 col-lg-4 text-center my-2">
+          <Rotate triggerOnce>
+            <div className="ourteam__image__wrapper">
+              <img
+                src={require("../../Assets/Images/ourteam_rojan-min.jpg")}
+                alt="rohan-pic"
+              />
+            </div>
+          </Rotate>
+          <Fade direction="up" delay={400}>
+            <h3>Rojan Pant</h3>
+          </Fade>
+        </Col>
+        <Col className="col-12 col-md-12 col-lg-4 text-center my-2">
+          <Rotate triggerOnce>
+            <div className="ourteam__image__wrapper">
+              <img
+                src={require("../../Assets/Images/ourteam_amrit-min.jpg")}
+                alt="rohan-pic"
+              />
+            </div>
+          </Rotate>
+          <Fade direction="up" delay={400}>
+            <h3>Amrit Gurung</h3>
+          </Fade>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
