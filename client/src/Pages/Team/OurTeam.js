be022a23d7256ae1d698e56eed0726890d437575
Rohan Magar <rohan.magar.415@gmail.com>
2022-12-10 10:08:55 +0545

    implemented particle animation

--- client/src/Pages/Team/OurTeam.js
+++ client/src/Pages/Team/OurTeam.js
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { BiArrowBack } from "react-icons/bi";
 import { Container, Row, Col } from "react-bootstrap";
 import { Rotate, Fade } from "react-awesome-reveal";
+import Particle from "../../Components/Particle/Particle";
 
 function OurTeam() {
   const navigate = useNavigate();
@@ -19,13 +20,14 @@ function OurTeam() {
     <Preloader />
   ) : (
     <Container fluid className="our_team">
+      <Particle />
       <span onClick={() => navigate(-1)}>
         <BiArrowBack style={{ marginTop: "-2px", marginRight: "5px" }} />
         Our Team
       </span>
       <Row>
         <Col className="col-12 col-md-6 col-lg-4 text-center my-2">
-          <Rotate triggerOnce>
+          <Rotate direction="top-left" triggerOnce duration={1500}>
             <div className="ourteam__image__wrapper">
               <img
                 src={require("../../Assets/Images/ourteam_rohan-min.jpg")}
@@ -33,12 +35,12 @@ function OurTeam() {
               />
             </div>
           </Rotate>
-          <Fade direction="up" delay={400}>
+          <Fade direction="up" duration={1200} delay={600}>
             <h3>Rohan Rana Magar</h3>
           </Fade>
         </Col>
         <Col className="col-12 col-md-6 col-lg-4 text-center my-2">
-          <Rotate triggerOnce>
+          <Rotate direction="top-left" triggerOnce duration={1500}>
             <div className="ourteam__image__wrapper">
               <img
                 src={require("../../Assets/Images/ourteam_rojan-min.jpg")}
@@ -46,12 +48,12 @@ function OurTeam() {
               />
             </div>
           </Rotate>
-          <Fade direction="up" delay={400}>
+          <Fade direction="up" duration={1200} delay={600}>
             <h3>Rojan Pant</h3>
           </Fade>
         </Col>
         <Col className="col-12 col-md-12 col-lg-4 text-center my-2">
-          <Rotate triggerOnce>
+          <Rotate direction="top-left" triggerOnce duration={1500}>
             <div className="ourteam__image__wrapper">
               <img
                 src={require("../../Assets/Images/ourteam_amrit-min.jpg")}
@@ -59,7 +61,7 @@ function OurTeam() {
               />
             </div>
           </Rotate>
-          <Fade direction="up" delay={400}>
+          <Fade direction="up" duration={1200} delay={600}>
             <h3>Amrit Gurung</h3>
           </Fade>
         </Col>
