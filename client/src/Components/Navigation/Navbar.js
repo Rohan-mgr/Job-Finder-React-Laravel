21d29e188a2bb9803488712432dd59498644eac3
Rohan Magar <rohan.magar.415@gmail.com>
2023-02-02 10:10:25 +0545

    added login page

--- client/src/Components/Navigation/Navbar.js
+++ client/src/Components/Navigation/Navbar.js
@@ -1,19 +1,36 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "./Navbar.css";
 import NavLink from "./NavLink/NavLink";
+import { BiLogIn, BiUserCircle } from "react-icons/bi";
+import { FaListUl } from "react-icons/fa";
 
 function NavigationBar() {
   const collapseRef = useRef(null);
+
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    function stickyNav() {
+      if (window.scrollY >= 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    }
+    window.addEventListener("scroll", stickyNav);
+    return () => {
+      window.removeEventListener("scroll", stickyNav);
+    };
+  }, []);
 
   const hideBars = () => {
     collapseRef.current.setAttribute("class", "navbar-collapse collapse");
   };
 
   return (
-    <Navbar bg="Navbar" expand="lg">
+    <Navbar bg="Navbar" className={show && "sticky-nav"} expand="lg">
       <Container fluid>
         <Navbar.Brand href="/">
           <img
@@ -25,17 +42,30 @@ function NavigationBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse
           id="basic-navbar-nav"
-          className="justify-content-center"
-          style={{ width: "100%" }}
+          // className="justify-content-center"
           ref={collapseRef}
         >
-          <Nav className="text-center my-2" style={{ marginLeft: "auto" }}>
+          <Nav className="text-center my-2">
             <NavLink Path="/" hideNav={hideBars} Link="Home" />
             <NavLink Path="about" hideNav={hideBars} Link="About" />
             <NavLink Path="login" hideNav={hideBars} Link="Login" />
             <NavLink Path="ourteam" Link="Our Team" />
           </Nav>
         </Navbar.Collapse>
+        <div className="user-menu">
+          <a href="#">
+            <FaListUl />
+            Post Job Free
+          </a>
+          <a href="#">
+            <BiUserCircle />
+            Sign Up
+          </a>
+          <a href="#">
+            <BiLogIn />
+            Login
+          </a>
+        </div>
       </Container>
     </Navbar>
   );
